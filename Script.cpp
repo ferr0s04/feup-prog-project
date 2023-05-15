13460a01e984c229ecf10af68f831381d3171c30
ferr0s04 <132689287+ferr0s04@users.noreply.github.com>
2023-05-15 13:36:57 +0100

    Update Script.cpp

--- Script.cpp
+++ Script.cpp
@@ -114,12 +114,47 @@ namespace prog {
     }
     void Script::invert() {
         // Each pixel (r, g, b) is transformed to (255 - r, 255 - g, 255 - b)
+        int width = image->width();
+        int height = image->height();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Color& pixel = (*image)(x, y);
+                pixel.red() = 255 - pixel.red();
+                pixel.green() = 255 - pixel.green();
+                pixel.blue() = 255 - pixel.blue();
+        }
+    }
     }
     void Script::toGrayScale() {
         // Each pixel (r, g, b) is transformed to (v, v, v), where v = (r + g + b)/3
+        int width = image->width();
+        int height = image->height();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Color& pixel = (*image)(x, y);
+                int gray = (pixel.red() + pixel.green() + pixel.blue()) / 3;
+                pixel.red() = gray;
+                pixel.green() = gray;
+                pixel.blue() = gray;
+        }
+    }
     }
     void Script::replace() {
         // Replace all (r1, g1, b1) by (r2, g2, b2)
+        int r1, g1, b1, r2, g2, b2;
+        input >> r1 >> g1 >> b1 >> r2 >> g2 >> b2;
+        int width = image->width();
+        int height = image->height();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Color& pixel = (*image)(x, y);
+                if (pixel.red() == r1 && pixel.green() == g1 && pixel.blue() == b1) {
+                    pixel.red() = r2;
+                    pixel.green() = g2;
+                    pixel.blue() = b2;
+            }
+        }
+    }
     }
     void Script::fill() {
     }
@@ -128,11 +163,82 @@ namespace prog {
     void Script::vMirror() {
     }
     void Script::add() {
+        string filename;
+        int r, g, b, x, y;
+        input >> filename >> r >> g >> b >> x >> y;
+
+        Image* addedImage = loadFromPNG(filename);
+        if (addedImage != nullptr && image != nullptr) {
+            int addedWidth = addedImage->width();
+            int addedHeight = addedImage->height();
+            int imageWidth = image->width();
+            int imageHeight = image->height();
+
+            for (int j = 0; j < addedHeight; j++) {
+                for (int i = 0; i < addedWidth; i++) {
+                    Color& srcPixel = (*addedImage)(i, j);
+                    int destX = x + i;
+                    int destY = y + j;
+                    if (destX >= 0 && destX < imageWidth && destY >= 0 && destY < imageHeight) {
+                        Color& destPixel = (*image)(destX, destY);
+                        if (srcPixel.red() != r || srcPixel.green() != g || srcPixel.blue() != b) {
+                            destPixel = srcPixel;
+                    }
+                }
+            }
+        }
+
+        delete addedImage;
+    }
     }
     void Script::crop() {
+        int x, y, w, h;
+        input >> x >> y >> w >> h;
+        if (image != nullptr) {
+            int imageWidth = image->width();
+            int imageHeight = image->height();
+            x = max(x, 0);
+            y = max(y, 0);
+            w = min(w, imageWidth - x);
+            h = min(h, imageHeight - y);
+            Image* croppedImage = new Image(w, h);
+            for (int j = 0; j < h; j++) {
+                for (int i = 0; i < w; i++) {
+                    Color& srcPixel = (*image)(x + i, y + j);
+                    Color& destPixel = (*croppedImage)(i, j);
+                    destPixel = srcPixel;
+            }
+        }
+        delete image;
+        image = croppedImage;
+    }
     }
     void Script::rotateLeft() {
+        int width = image->width();
+        int height = image->height();
+        Image* rotatedImage = new Image(height, width);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Color& srcPixel = (*image)(x, y);
+                Color& destPixel = (*rotatedImage)(height - 1 - y, x);
+                destPixel = srcPixel;
+            }
+        }
+        delete image;
+        image = rotatedImage;
     }
     void Script::rotateRight() {
+        int width = image->width();
+        int height = image->height();
+        Image* rotatedImage = new Image(height, width);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Color& srcPixel = (*image)(x, y);
+                Color& destPixel = (*rotatedImage)(y, width - 1 - x);
+                destPixel = srcPixel;
+            }
+        }
+        delete image;
+        image = rotatedImage;
     }
 }
