c95b17b73e351079e4535dc68d9b0d54ec9bf292
berno9 <132691904+berno9@users.noreply.github.com>
2023-05-16 16:44:27 +0100

    Update Color.cpp

--- Color.cpp
+++ Color.cpp
@@ -30,4 +30,44 @@ namespace prog {
     rgb_value& Color::blue()  {
       return blue_;
     }
+    
+    Color Color :: Median(const vector<Color>& v) {
+        Color newC;
+        vector<rgb_value> vColor;
+        for (int i = 0; i < v.size(); i++) {
+            vColor.push_back(v[i].red());
+        }
+        sort(vColor.begin(), vColor.end());
+        if (vColor.size() % 2 != 0) {
+            newC.red() = vColor[vColor.size() / 2];
+        } else {
+            newC.red() = (vColor[(vColor.size() / 2) - 1] + vColor[vColor.size() / 2]) / 2;
+        }
+
+        vColor.clear();
+
+        for (int i = 0; i < v.size(); i++) {
+            vColor.push_back(v[i].green());
+        }
+        sort(vColor.begin(), vColor.end());
+        if (vColor.size() % 2 != 0) {
+            newC.green() = vColor[vColor.size() / 2];
+        } else {
+            newC.green() = (vColor[(vColor.size() / 2) - 1] + vColor[vColor.size() / 2]) / 2;
+        }
+
+        vColor.clear();
+
+        for (int i = 0; i < v.size(); i++) {
+            vColor.push_back(v[i].blue());
+        }
+        sort(vColor.begin(), vColor.end());
+        if (vColor.size() % 2 != 0) {
+            newC.blue() = vColor[vColor.size() / 2];
+        } else {
+            newC.blue() = (vColor[(vColor.size() / 2) - 1] + vColor[vColor.size() / 2]) / 2;
+        }
+
+        return newC;
+    }
 }
