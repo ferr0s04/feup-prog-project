16fa71b963eb72fede50a7f3fd511a8947b7380f
berno9 <132691904+berno9@users.noreply.github.com>
2023-05-17 16:28:31 +0100

    Update Color.cpp

--- Color.cpp
+++ Color.cpp
@@ -1,26 +1,27 @@
 #include "Color.hpp"
+#include <vector>
+#include <algorithm>
+#include <iostream>
 
 namespace prog {
-    Color::Color() : red_(0), green_(0), blue_(0) {} // Default constructor = (0, 0, 0)
-    
-    Color::Color(const Color& other) : red_(other.red_), green_(other.green_), blue_(other.blue_) {} // Copy constructor
-    
+    Color::Color() : red_(0), green_(0), blue_(0) {} // Default Constructor = (0, 0, 0)
+
+    Color::Color(const Color& other) : red_(other.red_), green_(other.green_), blue_(other.blue_) {}  // Copy constructor
+
     Color::Color(rgb_value red, rgb_value green, rgb_value blue) : red_(red), green_(green), blue_(blue) {}
-    
-    // Funções apenas para leitura
+
     rgb_value Color::red() const {
         return red_;
     }
-    
     rgb_value Color::green() const {
         return green_;
     }
-    
     rgb_value Color::blue() const {
         return blue_;
     }
 
-    // Funções para leitura e escrita
+    // Usando os private fields:
+
     rgb_value& Color::red()  {
         return red_;
     }
@@ -30,35 +31,51 @@ namespace prog {
     rgb_value& Color::blue()  {
       return blue_;
     }
-    
-    Color Color :: Median(const vector<Color>& v) {                        
+
+    Color Color :: Median(const vector<Color>& v) {
         Color newC;
         vector<rgb_value> vColor;
-        for (int i = 0; i < v.size(); i++) {
+        
+        std::cout << "aqui";
+
+        for (int i = 0; i < int(v.size()); i++) {
             vColor.push_back(v[i].red());
         }
         sort(vColor.begin(), vColor.end());
-        newC.red() = vColor[vColor.size() / 2];
-        
 
+        if (vColor.size() % 2 != 0) {
+            newC.red() = vColor[vColor.size() / 2];
+        } else {
+            newC.red() = (vColor[(vColor.size() / 2) - 1] + vColor[vColor.size() / 2]) / 2;
+        }
+        
         vColor.clear();
 
-        for (int i = 0; i < v.size(); i++) {
+        for (int i = 0; i < int(v.size()); i++) {
             vColor.push_back(v[i].green());
         }
         sort(vColor.begin(), vColor.end());
-        newC.green() = vColor[vColor.size() / 2];
-        
+
+        if (vColor.size() % 2 != 0) {
+            newC.green() = vColor[vColor.size() / 2];
+        } else {
+            newC.green() = (vColor[(vColor.size() / 2) - 1] + vColor[vColor.size() / 2]) / 2;
+        }
+
 
         vColor.clear();
 
-        for (int i = 0; i < v.size(); i++) {
+        for (int i = 0; i < int(v.size()); i++) {
             vColor.push_back(v[i].blue());
         }
         sort(vColor.begin(), vColor.end());
-        newC.blue() = vColor[vColor.size() / 2];
         
+        if (vColor.size() % 2 != 0) {
+            newC.blue() = vColor[vColor.size() / 2];
+        } else {
+            newC.blue() = (vColor[(vColor.size() / 2) - 1] + vColor[vColor.size() / 2]) / 2;
+        }
 
-        return newC;                                      // Dado um vetor com um número de elementos ímpar do tipo Color, é feita a mediana de cada uma das cores (r, g, b)
+        return newC;                                      
     }
 }
