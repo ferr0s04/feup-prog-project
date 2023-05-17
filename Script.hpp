f069bf8156e85b5de3d86fa0dbd318266b384b2a
berno9 <132691904+berno9@users.noreply.github.com>
2023-05-17 16:30:32 +0100

    Update Script.hpp

--- Script.hpp
+++ Script.hpp
@@ -7,36 +7,36 @@
 
 namespace prog
 {
-  class Script
-  {
-  public: 
-    Script(const std::string &filename);
-    ~Script();
-    void run();
-  private:
-    // Current image.
-    Image *image;
-    // Input stream for reading script commands.
-    std::ifstream input;
-  private:
-    // Private functions
-    void clear_image_if_any();
-    void open();
-    void blank();
-    void save();
-    void invert();
-    void toGrayScale();
-    void replace();
-    void fill();
-    void hMirror();
-    void vMirror();
-    void add();
-    void crop();
-    void rotateLeft();
-    void rotateRight();
-    void medianFilter();
-    void XPM2_Open();
-    void XPM2_Save();
-  };
+    class Script
+    {
+    public:
+        Script(const std::string &filename);
+        ~Script();
+        void run();
+    private:
+        // Current image.
+        Image *image;
+        // Input stream for reading script commands.
+        std::ifstream input;
+    private:
+        // Private functions
+        void clear_image_if_any();
+        void open();
+        void blank();
+        void save();
+        void invert();
+        void toGrayScale();
+        void replace();
+        void fill();
+        void hMirror();
+        void vMirror();
+        void add();
+        void crop();
+        void rotateLeft();
+        void rotateRight();
+        void medianFilter();
+        void XPM2_Open();
+        void XPM2_Save();
+    };
 }
 #endif
